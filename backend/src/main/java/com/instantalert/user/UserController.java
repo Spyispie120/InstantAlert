f7ebaa672a734baeb0bbb0ef6e3443efab8c953f
PhuongTran <tranduyphuong2110@gmail.com>
2018-10-21 05:21:23 -0700

    preparedstatement not needed

--- backend/src/main/java/com/instantalert/user/UserController.java
+++ backend/src/main/java/com/instantalert/user/UserController.java
@@ -1,7 +1,6 @@
 package com.instantalert.user;
 
 import java.lang.invoke.MethodHandles;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import org.slf4j.Logger;
@@ -12,10 +11,8 @@ public class UserController {
 	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	public static void addUser(String username, String firstName, String lastName, String password) {
-		PreparedStatement pstmt = null;
 		//TODO: login feature needs deployed later
 		try{
-			
 			UserDao userDao = new UserDao();
 			if(!userDao.doesExist(username)){
 				
