766c8bf1b431ad7691bc5ce35245f402e4a183ac
PhuongTran <tranduyphuong2110@gmail.com>
2018-10-21 04:19:04 -0700

    UserController()

---
+++ backend/src/main/java/com/instantalert/user/UserController.java
@@ -0,0 +1,29 @@
+package com.instantalert.user;
+
+import java.lang.invoke.MethodHandles;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserController {
+
+	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	public static void addUser(String username, String firstName, String lastName, String password) {
+		PreparedStatement pstmt = null;
+		//TODO: login feature needs deployed later
+		try{
+			
+			UserDao userDao = new UserDao();
+			if(!userDao.doesExist(username)){
+				
+				//userDao.addUser(username, firstName, lastName, salt, hashedPassword);
+			}
+			
+		}catch(SQLException ex){
+			logger.error("error when creating GroupDao()." + ex.getMessage());
+		}
+	}
+}
